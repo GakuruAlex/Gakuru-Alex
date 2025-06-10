328f9b4280152802b6d28975bcb8f089bc41f28a
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-10 16:22:37 +0300

    right side component

--- src/body/Section.jsx
+++ src/body/Section.jsx
@@ -4,6 +4,7 @@ const Section = (props)=>{
 <div className={props.class_name}>
 {props.heading && (<h1>{props.heading}</h1>)}
 {props.sub_heading && (<h2>{props.sub_heading}</h2>)}
+{props.content_text && (<p>{props.content_text}</p>)}
  </div>   
  )
 }
