781e0f5512c73397843c4abfc027d0f5cd2f2fe9
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-11 16:36:41 +0300

    display certificate with description skills gained and verification link

--- src/components/certifications/CertificationComp.jsx
+++ src/components/certifications/CertificationComp.jsx
@@ -1,12 +1,22 @@
-
+import './CertificationComp.css'
 const CertificationComp = (props) => {
+;
 return (
     <>
     <div className="responsive">
   <div className="certificates">
     
-      <img src={props.img_link} alt={props.name} width="900" height="550"/>
-    <div className="desc">{props.description}</div>
+      <img src={props.img_link} alt={props.name}  className="certificate_img"/>
+    <div className="desc">
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <a href={props.verification_link} className="verify_link" target="_blank">Verify {props.name} </a>
+      <ul className='skills'>
+        <h3>Skills gained</h3>
+        {props.skills.map((skill, index)=><li key={index}>{skill}</li>)}
+      </ul>
+      </div>
+      
   </div>
 </div>
 
