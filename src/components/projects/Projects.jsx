b0d07a1d8ab8a19d3b743c0706617bfabef16623
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-11 18:20:33 +0300

    github link to project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,9 +8,24 @@ const Projects = () => {
         <>
         <h2>Projects</h2>
         <div className="projects-container">
-        <Project  name="FlashCard App"  description="" image_url={FlashCardApp} />
-        <Project name="Quiz App" description="" image_url={QuizApp}/>
-        <Project name="PasswordManager" description="" image_url={Passwordmanager}/>
+        <Project  
+        name="FlashCard App"
+        description="A FlashCardApp is a digital tool designed to help users create, study, and review flashcards for learning and memorization." 
+        image_url={FlashCardApp} 
+        github_link= "https://github.com/GakuruAlex/FlashCardApp"
+        />
+        <Project 
+        name="Quiz App" 
+        description="A Quiz App is an interactive application designed to test users' knowledge on various topics through true/false statements." 
+        image_url={QuizApp}
+        github_link = "https://github.com/GakuruAlex/quiz_app_v_2"
+        />
+        <Project
+         name="PasswordManager" 
+         description="A password manager is a software tool designed to securely store, manage, and autofill user credentials for various online accounts. " 
+         image_url={Passwordmanager}
+         github_link ="https://github.com/GakuruAlex/PasswordManager"
+         />
         </div>
 
         </>
