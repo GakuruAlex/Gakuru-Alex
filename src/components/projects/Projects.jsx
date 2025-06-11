15ef66e9de38abb37f3aa2cfad1e978e257bc37f
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-11 17:56:07 +0300

    add project prop

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,18 @@
+import './Projects.css'
+import FlashCardApp from "./flashcard app.png"
+import QuizApp from "./quiz app.png"
+import Passwordmanager from "./password manager.png"
+import Project from "./Project"
 const Projects = () => {
     return (
         <>
         <h2>Projects</h2>
+        <div className="projects-container">
+        <Project  name="FlashCard App"  description="" image_url={FlashCardApp} />
+        <Project name="Quiz App" description="" image_url={QuizApp}/>
+        <Project name="PasswordManager" description="" image_url={Passwordmanager}/>
+        </div>
+
         </>
     )
 }
