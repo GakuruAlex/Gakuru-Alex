40ee5851511092a09f39cc26f5858e2e98d4e2c5
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-11 12:27:52 +0300

    cv

--- src/body/Resume.jsx
+++ src/body/Resume.jsx
@@ -1,11 +1,11 @@
 import './Resume.css'
-
+import CV from '../../public/Alex_Gakuru_CV.pdf'
 const Resume = ()=>{
-    const google_driveId = "1aEKw42V3Yw1SKJ3fSttMTwzVZDp4Ud8N"
+
  return (
   <>
         <div style={{ width: '50%', height: '85vh', border: '1px solid #ddd' }} className='pdf-viewer'>
-        <embed src="Gakuru-Alex/Alex_Gakuru_CV.pdf" width="100%" height="100%" type="application/pdf" />
+        <embed src={CV} width="100%" height="100%" type="application/pdf" />
       </div>
    
     </>
