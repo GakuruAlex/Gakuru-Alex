abe63e78f193abf26d2430f46e429e8c2bdf1054
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-21 23:56:37 +0300

    create a resume page with a profile component

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,18 @@
+import "../css/resume.css"
+import "../css/profile.css"
+import Profile from "./Profile"
+const Resume = ()=>{
+    return (
+        <>
+        <div className="container">
+        
+        <div className="profile-container"><Profile/></div>
+        <div className="content">
+        <p>Hello</p>
+        </div>
+        </div>
+        </>
+    )
+
+}
+export default Resume
