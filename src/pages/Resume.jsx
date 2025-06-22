39f18d7f59a4cb293bb868553714e0b5b4614908
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-22 23:37:35 +0300

    work experience section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -8,14 +8,27 @@ const Resume = ()=>{
         
         <div className="profile-container"><Profile/></div>
         <div className="content">
-        <div className="education">
-            <h2>Education</h2>
-            <div> <p>Karatina University</p>
-                 <p className="degree-date">Aug 2017 -  Dec 2021</p>
-            </div>
+            <div className="professional-summary">
+                <h2 className="professional-summary-title">Professional Sumary:</h2>
            
-            <p className="degree-course">BSc in Computer Science</p>
-        </div>
+                <p className="professional-summary-body">   Computer Scientist passionate about software development, with a strong focus on software security. 
+                </p>
+            </div>
+            <div className="education">
+                <h2>Education</h2>
+                <div> <p> School: Karatina University</p>
+                 <p className="degree-date">Aug 2017 -  Dec 2021</p>
+                    
+            
+                </div>
+                <p className="degree-course"> Degree: BSc in Computer Science</p>
+            </div>
+            <div className="professional-experience">
+                <h2 className="work-experience-title">Work Experience:</h2>
+
+          
+             </div>
+
         </div>
         </div>
         </>
