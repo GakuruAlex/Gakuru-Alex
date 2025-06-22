33325ddc58635cee7509b246e901867a7670aace
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-23 00:37:40 +0300

    import and run exerience component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,29 @@
 import "../css/resume.css"
 import "../css/profile.css"
 import Profile from "./Profile"
+import Experience from "../components/Experience"
 const Resume = ()=>{
+        const experiences = [
+            <Experience 
+                title="State Department for Basic Education"
+                title_class= "moe-experience"
+                duration= "May 2024 - May 2025"
+                location = "Naivasha"
+                duties = {[
+
+                    "Trained teachers from 30 primary schools on how to use  NetSupport in teaching.",
+                    "Conducted 50+ in-classroom support sessions for teachers  on technology integration.",
+                    "Achieved a 90% teacher satisfaction rate for in-classroom  technology support.",
+                    "Provided direct support to 100% of schools participating in  the Digital Literacy Programme in the region.",
+                    "Trained 50 teachers on best practices for safe and ethical  technology use.", 
+                    "Resolved over 100 first-line ICT support requests for staff."
+                ]}
+            />,
+        ]
     return (
+
         <>
+    
         <div className="container">
         
         <div className="profile-container"><Profile/></div>
@@ -25,8 +45,13 @@ const Resume = ()=>{
             </div>
             <div className="professional-experience">
                 <h2 className="work-experience-title">Work Experience:</h2>
-
-          
+                
+                    {experiences.map((experience, index) =>(
+                        <div key={index}>
+                        {experience}
+                        </div>
+                        ))
+                        }
              </div>
 
         </div>
