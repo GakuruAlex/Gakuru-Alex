3a099b02e4fcc6c1f39d3710c9ade687e4033bc6
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-24 01:48:54 +0300

    certification container

---
+++ src/pages/Certifications.jsx
@@ -0,0 +1,15 @@
+import "../css/certifications.css"
+import Profile from "./Profile";
+
+const Certifications = ()=>{
+    return (
+        <div className="certifications-container">
+            <Profile/>
+                <div className="">
+
+                </div>
+        </div>
+    )
+}
+
+export default Certifications;
