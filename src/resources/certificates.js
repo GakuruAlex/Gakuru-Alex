fd7065406f5eaf969fe1bea58b389c382c083ef5
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-24 18:19:41 +0300

    certificates dictionary

---
+++ src/resources/certificates.js
@@ -0,0 +1,14 @@
+import csharp from "../assets/Foundational_CSharp.png" 
+const certificates = {
+    1: {
+        course: "Foundational C# with Microsoft",
+        school: "Microsoft and freeCodeCamp",
+        skills: [
+            "Object Oriented Programming", "C#", "Debugging C# apps", "Using control structures"
+        ],
+        credential_link: "https://freecodecamp.org/certification/alexgakuru/foundational-c-sharp-with-microsoft",
+        image_url: {csharp}
+    }
+}
+
+export default certificates;
