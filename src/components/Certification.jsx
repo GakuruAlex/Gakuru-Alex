41748feea61d6bfaa3279b647cb1e15c4ebd8284
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-26 16:01:49 +0300

    create a certification container

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -1,21 +1,29 @@
 import "../css/certification.css"
-const Certification = ({course, school, skills, credential_link, image_url}) =>{
+const Certification = ({course, school, skills, credential_link, image_url, nextButton, prevButton}) =>{
     
 return (
+    <div className="certification-container">
     <div className="certification">
+        {nextButton}
      <img src={image_url} alt={course} className="certificate-img"/>
-     <div className="certification-details">
-        <div className="details">
-        <h2 className="course">Course: {course}</h2>
-        <h3 className="school">Institution: {school}</h3>
-        <p>Verification link <a href={credential_link} className="verify-link">{course} </a></p>
-        </div>
-       
-        <ul className="skills">
+     {prevButton}
+    
+          <ul className="skills">
+             <p>Skills:</p>
             {skills.map((skill, index)=>(
                 <li key={index}>{skill}</li>
         ))}
         </ul>
+
+    </div>
+         <div className="certification-details">
+        <div className="details">
+        <h2 className="course">Course: {course}</h2>
+        <h3 className="school">Institution: {school}</h3>
+        <a href={credential_link} className="verify-link" target="_blank"> Verification link</a>
+        </div>
+       
+   
        
      </div>
     </div>
