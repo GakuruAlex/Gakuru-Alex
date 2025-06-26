59d337810dcd1471f8a019908faf96a6b2eec2a0
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-26 22:40:20 +0300

    define projects array

---
+++ src/resources/projects.js
@@ -0,0 +1,26 @@
+import Get_Right from "../assets/quiz_get_right.png";
+import Get_Wrong from "../assets/quiz_get_wrong.png";
+
+
+const projects = [
+    {
+        "id": 0,
+        "name": "Quiz App",
+        "git_url": "https://github.com/GakuruAlex/quiz_app_v_2",
+        "details": "This ia quiz project. The questions are fetched from Open Trivia Api with params: amount=10, category=18 and type=boolean. Its a True or False system. True is marked by a button with ✓ and False with 'x'. If the answer is correct the canvas changes to green else red before the next question and score is increased by one.",
+        "image_urls": [Get_Right, Get_Wrong],
+        "stack": ["Python", "API", "GitHub"]
+    },
+    {
+        "id": 1,
+        "name": "FlashCardApp",
+        "details": "This Python application helps you learn a new language by displaying flashcards with the word in one language on the front and its translation in the other language on the back. You can customize the application with your own flashcards stored in a CSV file.",
+        "image_urls": [],
+        "git_url": "https://github.com/GakuruAlex/FlashCardApp",
+        "stacks": ["Python", "GitHub"]
+
+
+    }
+]
+
+export default projects;
