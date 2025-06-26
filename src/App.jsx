d60e597e635fafc8cb6c15d99354080ddc81d3d2
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-26 16:05:15 +0300

    add Project Labs and About routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Route, Routes, unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
-import "./typography.css"
+import "./typography.css";
 import Certifications from './pages/Certifications';
 import { createBrowserHistory } from "history";
 import HomePage from './pages/HomePage';
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Labs from "./pages/Labs";
 const history = createBrowserHistory({ basename: "/Gakuru-Alex" });
 function App() {
   return (
@@ -11,10 +14,11 @@ function App() {
         <Routes>
         <Route path="/Gakuru-Alex/" element={<HomePage />} />
         <Route path="/Gakuru-Alex/certifications" element={<Certifications />} />
-        {/* <Route path="/Gakuru-Alex/projects" element={<Projects />} />
-        
-        <Route path="/Gakuru-Alex/labs" element={<LabsProject projects= {projects} />} />
-        <Route path="/Gakuru-Alex/contact" element={<Contact />} /> */}
+        <Route path="/Gakuru-Alex/projectss" element={<Projects />} />
+        <Route path="/Gakuru-Alex/labs" element={<Labs />} />
+        <Route path="/Gakuru-Alex/about" element={<About />} />
+
+
 
       </Routes>
         </HistoryRouter>
