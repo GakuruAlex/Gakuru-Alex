118649650a14e6dd68cbc2b60604fe8c7bf52cac
GakuruAlex <gakurualexgicimo@gmail.com>
2025-06-30 23:46:30 +0300

    next and prev button

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,8 @@ import "../css/projects.css";
 import allProjects from "../resources/allProjects";
 import { useState, useEffect } from "react";
 import Project from "../components/Project";
+import Next from "../assets/next.png";
+import Prev from "../assets/previous.png";
 
 const Projects = () =>{
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -21,7 +23,8 @@ const Projects = () =>{
 
             <div className="projects-container">
                 <NavBar/>
-                <button className="next" onClick={()=>setCurrentIndex((Index) => Index + 1)}>&#10095;</button>
+                <div className="row-container">
+                <a className="next-prev" onClick={()=>setCurrentIndex((Index) => Index + 1)}><img src={Next} alt="next" /></a>
                  
 
                 <Project 
@@ -33,8 +36,10 @@ const Projects = () =>{
                 stacks={currentProject["stacks"]}
                 />
 
-              <a className="prev" onClick={()=>setCurrentIndex((currentIndex) => currentIndex - 1)}>&#10094;</a>
+              <a className="next-prev" onClick={()=>setCurrentIndex((currentIndex) => currentIndex - 1)}><img src={Prev} alt="prev" /></a>
 
+                </div>
+               
 
 
 
