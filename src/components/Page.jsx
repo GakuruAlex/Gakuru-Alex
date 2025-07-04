11d1bf4d8ca8c3e165128d66dab723ecac00eb80
GakuruAlex <gakurualexgicimo@gmail.com>
2025-07-05 01:16:56 +0300

    Page component skeleton

---
+++ src/components/Page.jsx
@@ -0,0 +1,10 @@
+const Page = ({school, skills, verify_link,  name, details, image_urls, page_name})=>{
+    return(
+        <div className={page_name}>
+
+        </div>
+    )
+
+}
+
+export default Page;
